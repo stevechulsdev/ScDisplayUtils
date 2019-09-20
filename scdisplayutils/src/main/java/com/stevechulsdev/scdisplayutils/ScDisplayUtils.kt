96563543d8e6b4d472082037b3b18e6f88585cb6
stevechulsdev <stevechulsdev@gmail.com>
2019-09-20 14:57:16 +0900

    change progressbar background color

--- scdisplayutils/src/main/java/com/stevechulsdev/scdisplayutils/ScDisplayUtils.kt
+++ scdisplayutils/src/main/java/com/stevechulsdev/scdisplayutils/ScDisplayUtils.kt
@@ -49,6 +49,7 @@ object ScDisplayUtils {
         else {
             mProgressBar = ProgressBar(activity.applicationContext, null, android.R.attr.progressBarStyle).apply {
                 isIndeterminate = true
+                setBackgroundColor(ContextCompat.getColor(activity, android.R.color.transparent))
                 visibility = View.VISIBLE
 
                 try {
